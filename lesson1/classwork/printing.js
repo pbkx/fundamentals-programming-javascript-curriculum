27335364dde521e4811a353277fb87a6e3d1dd10
steamproof <93405617+pbkx@users.noreply.github.com>
2026-03-22 17:57:50 -0700

    Finished Lesson 1

---
+++ lesson1/classwork/printing.js
@@ -0,0 +1,6 @@
+console.log("Hello, world!");
+console.log("My name is Max.");
+console.log('My favorite food is pizza.');
+
+// Error: quatation marks mismatched
+// console.log('This doesnt work");
---
+++ lesson1/classwork/variables.js
@@ -0,0 +1,10 @@
+let x = 5;  // number: whole-number value
+console.log(x);
+
+let number = 3.14;  // number: decimal value
+console.log(number);
+
+let favorite_food = "pizza";  // string: text
+console.log("My favorite food is " + favorite_food);
+
+console.log(3.18 + " dogs " + x);  // string concatenation
